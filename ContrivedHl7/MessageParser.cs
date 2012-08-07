85fc5c1464efaf7ada3dbdf471dbeffc0e190175
Thomas Yarnall <thomas.yarnall@softekinc.com>
2012-08-07 09:34:18 -0500

    Added actual Hl7 path parsing

--- ContrivedHl7/MessageParser.cs
+++ ContrivedHl7/MessageParser.cs
@@ -7,9 +7,15 @@ namespace ContrivedHl7
 {
    public class MessageParser
    {
-      public static string Parse(string name, int index, int field)
+      public static string Parse(string name, int index, int field, int repetition, int component, int subcomp)
       {
-         return string.Format("Do something with...\nname: {0}\nindex: {1}\nfield: {2}", name, index, field);
+         return string.Format("Do something with...\n" +
+                              "name: {0}\n" +
+                              "index: {1}\n" +
+                              "field: {2}\n" +
+                              "repeat: {3}\n" +
+                              "component: {4}\n" +
+                              "subcomp: {5}", name, index, field, repetition, component, subcomp);
       }
    }
 }
